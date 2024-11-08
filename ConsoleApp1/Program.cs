3393786e28a0ef45ee6a3e1c5523653dec0ff123
joaosilva57 <joaosilva.202058@epff-intep.pt>
2024-11-08 13:00:52 +0000

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+public class programfirst
+{
+    public static void Main(string[] args)
+    {
+        int idade = 15;
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("pode3 entra na beber uma ceveja!");
+        }
+        else if (idade < 18 && idade >= 15)
+        {
+            Console.WriteLine("pode entar mas sò bebe sumo");
+        }
+        else
+        {
+            Console.WriteLine("pode entar na discoteca");
+        }
+    }
+}
